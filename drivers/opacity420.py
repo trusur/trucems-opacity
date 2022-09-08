14925c1c45faa2fb1894ce9bad2e831a622b4e17
ir001 <irwanantonio2708@gmail.com>
2022-09-08 10:57:34 +0700

    add: drivers opacity

---
+++ drivers/opacity420.py
@@ -0,0 +1,83 @@
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import time
+import random
+import json
+import requests
+import logging
+import datetime
+
+date = datetime.datetime.now()
+errorFile = "error_logs/"+str(date.strftime("%d_%m_%y"))+'_errors.log'
+print(errorFile)
+logging.basicConfig(filename=errorFile, filemode='w', format='%(asctime)s - %(message)s')
+baseUrl = "http://localhost/trucems-opacity/public/{}"
+portADC = 'COM3'
+portDAC = 'COM4'
+
+def updateValue(sensorId, value):    
+    try:
+        url = baseUrl.format("/api/sensor-value/"+str(sensorId))
+        payload='value='+str(value)
+        response = requests.request("PATCH", url, headers={
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }, data=payload)
+        return json.loads(response.text)
+    except Exception as e:
+        logging.error(e)
+        return False
+    
+def getSensorValue(sensorId):
+    try:
+        response = requests.request("GET", baseUrl.format("/api/sensor-value/"+str(sensorId)))
+        return json.loads(response.text)
+    except Exception as e:
+        logging.error(e)
+        return False
+    
+def getAnalogInput():
+    try:
+        clientInput = ModbusClient(method = 'rtu',port=portADC,baudrate=9600,parity = 'N',timeout=3)
+        if(clientInput.connect()):
+            read = clientInput.read_holding_registers(0, 8, unit=1)
+            clientInput.close()
+            return read.registers[0]
+        return -1
+    except Exception as e:
+        logging.error(e)
+        return -1
+
+def setAnalogOutput(outputIndex, value):
+    if(value > 4000 and value < 20000):
+        value = value
+    elif value < 4000:
+        value = 4000
+    elif value > 20000:
+        value = 20000
+    value = int(value)
+    print(value)
+    try:
+        client = ModbusClient(method = 'rtu',port=portDAC,baudrate=9600,parity = 'N',timeout=3)
+        connection = client.connect()
+        if(connection):
+            write = client.write_register(outputIndex, value, unit=1)
+            client.close()
+            return True
+        
+        return False
+    except Exception as e:
+        logging.error(e)    
+        return False
+sensors = [1]
+while True:    
+    try:
+        for sensorId in sensors:
+            sensor = getSensorValue(sensorId)
+            value = getAnalogInput()
+            concentrate = -1 if value == -1 else eval(sensor['sensor']['read_formula'])
+            time.sleep(1)
+            updateValue(sensorId,concentrate)
+            # setAnalogOutput(0,value)
+            time.sleep(1)
+        time.sleep(1)
+    except Exception as e:
+        logging.error(e)
--- drivers/runtime.py
+++ drivers/runtime.py
@@ -6,10 +6,8 @@ import json
 while True:
     try:
         now = datetime.now()
-        patch_url_runtime = "http://localhost/trucems/public/api/runtime"
-        # payload
+        patch_url_runtime = "http://localhost/trucems-opacity/public/api/runtime"
         get_payload = {}
-        # headers
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -22,24 +20,18 @@ while True:
                 days = float(json_get_runtime["data"]["days"]) + 1
                 hours = 0
                 minutes = 0
-                # print("days")
             elif(int(json_get_runtime["data"]["minutes"]) >= 59):
                 days = json_get_runtime["data"]["days"]
                 hours = float(json_get_runtime["data"]["hours"]) + 1
                 minutes = 0
-                # print("hour")
             else:
                 days = json_get_runtime["data"]["days"]
                 hours = json_get_runtime["data"]["hours"]
                 minutes = float(json_get_runtime["data"]["minutes"]) + 1
-                # print("minutes")
             patch_payload_runtime = 'days=' + \
                 str(days)+'&hours='+str(hours)+'&minutes='+str(minutes)
             response = requests.request(
                 "PATCH", patch_url_runtime, headers=headers, data=patch_payload_runtime)
-            # print(json.loads(response.text))
-        else:
-            do_nothing = ''
     except Exception as e:
-        do_nothing = ''
-        # print("[X]  Not connected ", e)
+        now2 = datetime.now()
+        timestamp = now2.strftime("%Y-%m-%d %H:%M:%S")
